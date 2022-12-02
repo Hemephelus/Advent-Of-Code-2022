8ed7adc04ebbb0bade723641e91deed35d26a126
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-02 13:05:53 +0100

    day02p1 done

--- solution.py
+++ solution.py
@@ -1,13 +1,37 @@
 # Import here
+from support import txt_to_D_list
 
 # Write Code here
 def output():
-
-    result = ''#function
+    input = txt_to_D_list()
+    result = Rock_Paper_Scissors(input)
     print(result)
 
 #function here
+def Rock_Paper_Scissors(input):
+    RPS_score = {
+        'X': 1,
+        'Y': 2,
+        'Z': 3,
+    }
 
+    W_D_L = {
+        'A X': 3,
+        'B X': 0,
+        'C X': 6,
+        'A Y': 6,
+        'B Y': 3,
+        'C Y': 0,
+        'A Z': 0,
+        'B Z': 6,
+        'C Z': 3,
+    }
+    b = 0
+    for i in input:
+      a =  W_D_L[i] + RPS_score[i.split(' ')[1]]
+      b += a
+
+    return b
 
 # output function
 output()
