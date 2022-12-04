53c07fde7269fcc6e50353fa0be9f1c2bff391db
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-04 18:43:36 +0100

    day04p2 done

--- solution.py
+++ solution.py
@@ -15,17 +15,20 @@ def Camp_Cleanup(pairs):
         p = pair.split(',')
         p1 = p[0].split('-')
         p2 = p[1].split('-')
-        min_value = str(min(int(p1[0]),int(p2[0])))
-        max_value = str(max(int(p1[1]),int(p2[1])))
-        
-        if min_value in p1 and  max_value in p1:
-            count += 1
-            c += 1
-        if min_value in p2 and  max_value in p2:
-            count += 1
-            c += 1
-        if c == 2:
+
+        for i in range(int(p1[0]),int(p1[1])+1):
+            if str(i) in p2:
+                count += 1
+                c += 1
+                break
+        for i in range(int(p2[0]),int(p2[1])+1):
+            if str(i) in p1:
+                count += 1
+                c += 1
+                break
+        if c > 1:
             count -= 1
+
        
     return count
 
