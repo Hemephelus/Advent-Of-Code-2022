adc935cf6ad9764d2cd798052cf2a8331e8fa6b4
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-06 12:29:51 +0100

    day06p1

--- solution.py
+++ solution.py
@@ -1,12 +1,38 @@
 # Import here
-
+from support import txt_to_string
 # Write Code here
 def output():
-
-    result = ''#function
+    data_stream = txt_to_string()
+    result = Tuning_Trouble(data_stream)
     print(result)
 
 #function here
+def Tuning_Trouble(data_stream):
+    start_of_packet = []
+    marker_char = 0
+    marker = False
+    for j,section  in enumerate(data_stream):
+        start_of_packet = data_stream[j:4+j]
+        print(start_of_packet)
+        packet = {}
+        
+        for i in start_of_packet:
+            if packet.get(i) == None:
+                packet[i] = 1
+            else:
+                packet[i] += 1
+            if packet[i] >= 2:
+                marker = False
+                break
+            else:
+                marker = True
+        if marker:
+            break
+
+        marker_char += 1
+
+
+    return marker_char + 4
 
 
 # output function
