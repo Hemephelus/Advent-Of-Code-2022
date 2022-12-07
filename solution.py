6434f66ccb7a7c79429c28c0ee868bcab5c9fdc7
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-07 07:22:02 +0100

    added a test input file

--- solution.py
+++ solution.py
@@ -1,8 +1,12 @@
 # Import here
+from support import txt_to_D_list,txt_to_string
 
 # Write Code here
 def output():
+    # "input.txt"
+    # "test_input.txt"
 
+    
     result = ''#function
     print(result)
 
--- support.py
+++ support.py
@@ -1,11 +1,11 @@
-def open_file():
-    f = open("input.txt", "r")
+def open_file(input):
+    f = open(input, "r")
     return f.read()
 
-def txt_to_string():
-    return open_file()
+def txt_to_string(input):
+    return open_file(input)
 
-def txt_to_D_list():
-    D_List = open_file()
+def txt_to_D_list(input):
+    D_List = open_file(input)
     D_List = D_List.split('\n')
     return D_List
