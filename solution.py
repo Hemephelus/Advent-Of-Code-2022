0a2811579b71ba9e246ee83813227cedef3b92b2
Hemephelus <nwachukwuujubuonu@gmail.com>
2022-12-10 17:24:47 +0100

    day09p2

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ def Rope_Bridge(moves):
     visited_places = 0
     origin = (0,0)
     is_diagonal = False
-
+    v = set([(0, 0)])
     directions = {
         'R':(1,0),
         'L':(-1,0),
@@ -21,43 +21,52 @@ def Rope_Bridge(moves):
         'D':(0,-1),
     }
 
-    head_list = []
-    tail_list = []
-    
-    tail_list.append(origin)
-    head_list.append(origin)
+    rope = []
+    R = [[0, 0] for _ in range(10)]
+
+    for i in range(10):
+        rope.append([])
+        rope[i].append(origin)
+
 
     for move in moves:
         direction = move.split(' ')[0]
         step_num = int(move.split(' ')[1])
 
         for i in range(step_num):
-            current_head_x = head_list[-1][0] + directions[direction][0]
-            current_head_y = head_list[-1][1] + directions[direction][1]
-            head_list.append((current_head_x,current_head_y))
 
-            x_diff = abs(head_list[-1][0] - tail_list[-1][0])
-            y_diff = abs(head_list[-1][1] - tail_list[-1][1])
+            current_head_x = rope[0][-1][0] + directions[direction][0]
+            current_head_y = rope[0][-1][1] + directions[direction][1]
+
+            R[0][0] += current_head_x
+            R[0][1] += current_head_y
+
+            # rope[0].append((current_head_x,current_head_y))
+
+            for j in range(9):
+                H = R[j]
+                T = R[j + 1]
+               
+                x_diff = H[0] - T[0]
+                y_diff = H[1] - T[1]
+
+                if abs(x_diff) > 1 or abs(y_diff) > 1:
+                    if x_diff == 0:
+                        T[1] += y_diff // 2
+                    elif y_diff == 0:
+                        T[0] += x_diff // 2
+                    else:
+                        T[0] += 1 if x_diff > 0 else -1
+                        T[1] += 1 if y_diff > 0 else -1
+        
+            # rope[j+1].append(rope[j][-2])
+            v.add(tuple(R[-1]))
+        # print(rope)
+        # return
             
 
-            if x_diff + y_diff <= 1:
-                is_diagonal = False
-                continue
 
-            if x_diff == 1 and  y_diff == 1:
-                is_diagonal = True
-                continue
-            
-            if is_diagonal: 
-                is_diagonal = False
-      
-
-
-            tail_list.append(head_list[-2])
-            
-
-
-    visited_places = len(set(tail_list))
+    visited_places = len(v)
 
 
 
